cc5b3e9a0d7916a7cda4c19ba544a80136784714
Bastien NICOLAU <bastien.nicolau@univ-amu.fr>
2021-10-20 20:21:44 +0200

    Setup du projet

---
+++ src/Controller/CryptobookController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class CryptobookController extends AbstractController
+{
+    /**
+     * @Route("", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('cryptobook/index.html.twig', [
+            'controller_name' => 'CryptobookController',
+        ]);
+    }
+}
